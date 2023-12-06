865bd397a3a19f4d8e41926482e94132892ee1cb
Cristian Lopez <analistadesarrollosapfront2_lem@castores.com.mx>
2023-12-06 09:27:34 -0600

    Correccionlogin

--- src/app/components/login/recuperacion-login/recuperacion-login.component.ts
+++ src/app/components/login/recuperacion-login/recuperacion-login.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { AuthServiceService } from 'src/app/service/auth-service.service';
 
 @Component({
   selector: 'app-recuperacion-login',
   templateUrl: './recuperacion-login.component.html',
-  styleUrls: ['./recuperacion-login.component.scss']
+  styleUrls: ['./recuperacion-login.component.scss'],
 })
 export class RecuperacionLoginComponent {
+  constructor(public apiService: AuthServiceService, public router: Router) {}
 
+  correo: string = '';
+  mensajeError: string = '';
+
+  iniciarSesion() {
+    // if (!this.correo ) {
+    //   return;
+    // }
+    // this.apiService.iniciarSesion(this.correo).subscribe(
+    //   (response) => {
+    //     if (response.estatus != 1) {
+    //     } else {
+    //       sessionStorage.setItem('sesionIniciada', 'true');
+    //       sessionStorage.setItem('usuario', response.data);
+    //       this.router.navigate(['/login']);
+    //     }
+    //   },
+    //   (error) => {
+    //     console.error('Error al iniciar sesión', error);
+    //     this.mensajeError = 'Ocurrió un error al iniciar sesión. Inténtalo de nuevo más tarde.';
+    //   }
+    // );
+  }
+
+  public get validarLogin() {
+    if (this.correo != '' && this.correo.includes('@gmail.com')) return false;
+
+    return true;
+  }
 }
--- src/app/components/login/registro-login/registro-login.component.ts
+++ src/app/components/login/registro-login/registro-login.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthServiceService } from 'src/app/service/auth-service.service';
 
 @Component({
   selector: 'app-registro-login',
   templateUrl: './registro-login.component.html',
-  styleUrls: ['./registro-login.component.scss']
+  styleUrls: ['./registro-login.component.scss'],
 })
 export class RegistroLoginComponent {
+  constructor(private apiService: AuthServiceService, private router: Router) {}
 
+  correo: string = '';
+  contrasenia: string = '';
+  confirmarContrasenia = '';
+  mensajeError: string = '';
+  usuario: string = '';
+
+  registrar() {
+    if (!this.correo || !this.contrasenia) {
+      return;
+    }
+    this.apiService.iniciarSesion(this.correo, this.contrasenia).subscribe(
+      (response) => {
+        if (response.estatus != 1) {
+        } else {
+          sessionStorage.setItem('sesionIniciada', 'true');
+          sessionStorage.setItem('usuario', response.data);
+          this.router.navigate(['/login']);
+        }
+      },
+      (error) => {
+        console.error('Error al iniciar sesión', error);
+        this.mensajeError =
+          'Ocurrió un error al iniciar sesión. Inténtalo de nuevo más tarde.';
+      }
+    );
+  }
+
+  public get validarLogin() {
+    if (
+      this.correo != '' &&
+      this.correo.includes('@gmail.com') &&
+      this.contrasenia != '' &&
+      this.confirmarContrasenia != ''
+    )
+      return false;
+
+    return true;
+  }
 }
