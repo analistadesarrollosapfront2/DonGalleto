fee532bd2c0f0b328f9970ce5e341c7845ebf9ec
Daniel Salazar <dsalazar@grupoinndex.com.mx>
2023-12-08 09:24:17 -0600

    x

--- src/app/components/control-productos/control-productos.component.ts
+++ src/app/components/control-productos/control-productos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ControlProductosService } from 'src/app/service/control-productos/control-productos.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-control-productos',
@@ -10,9 +11,13 @@ export class ControlProductosComponent {
   value: any | undefined;
   items: any[] = [];
   productos: any[] = [];
+  visible2 : boolean = false;
+  visible3 : boolean = false;
+  nombre: string = "";
+  nombreEditar: string = "";
 
   constructor(
-    private productoService: ControlProductosService,
+    private productoService: ControlProductosService, private messageService: MessageService
   ) { }
 
 
@@ -27,9 +32,10 @@ export class ControlProductosComponent {
         icon: 'pi pi-book',
         command: () => {
           console.log('Habrir ');
+          this.showDialog()
         },
         tooltipOptions: {
-          tooltipLabel: 'Reporte',
+          tooltipLabel: 'Agregar',
         },
       },
       {
@@ -72,5 +78,63 @@ export class ControlProductosComponent {
       this.getProductos();
     }
   }
+
+  showDialog(){
+    this.visible2 = true;
+  }
+
+  showDialog2(){
+    this.visible3 = true;
+  }
+
+  showModal(){
+    this.visible2 = true;
+  }
+
+  hideModal(){
+    this.visible2 = false;
+  }
+
+  showMessageSuccess(mensaje:string){
+    this.messageService.add({ key: 'toast', severity: 'success', summary: 'Exitoso', detail: mensaje, life: 3000 });
+    this.messageService.clear();
+  }
+
+  showMessageError(mensaje:string){
+    this.messageService.add({ key: 'toast', severity: 'error', summary: 'Error', detail: mensaje, life: 3000 });
+  }
   
+  registrarProducto(){
+    this.productoService.postProducto(this.nombre).subscribe(
+      (response) => {
+        console.log(response)
+        
+        this.getProductos();
+        this.hideModal();
+
+      },
+      (error) => {
+        console.error('Error al obtener ventas:', error);
+      }
+    );
+    
+  }
+
+  editarProducto(){}
+  
+
+  eliminarProducto(id:number){
+    this.productoService.deleteProductos(id).subscribe(
+      (response) => {
+        console.log(response)
+        
+        this.getProductos();
+
+      },
+      (error) => {
+        console.error('Error al obtener ventas:', error);
+      }
+    );
+  }
+
 }
--- src/app/service/control-productos/control-productos.service.ts
+++ src/app/service/control-productos/control-productos.service.ts
@@ -1,17 +1,49 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Max-Disk-Usage': '10mb', // Establece el límite de carga aquí
+  }),
+  timeout: 20000,
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControlProductosService {
 
-  constructor(private http: HttpClient) { }  
+  constructor(private http: HttpClient) { } 
+  
 
 
   getProductos() {
     const url = 'http://localhost:3000/api/productos/listarProductos/';
     return this.http.get<any>(url);
+  }
+
+  getProducto(id:number) {
+    const url = 'http://localhost:3000/api/productos/obtenerUnProducto/${id}';
+    return this.http.get<any>(url);
+  }
+
+  postProducto(nombre:string){
+    const url = 'http://localhost:3000/api/productos/insertarProducto/';
+    const body = { nombre };
+    return this.http.post<any>(url, body, httpOptions);
+  }
+
+  putProductos(id: number, nombre:string){
+    const url = 'http://localhost:3000/api/productos/actualizarProducto/';
+    const body = { id, nombre };
+    return this.http.put<any>(url, body, httpOptions);
+  }
+
+  deleteProductos(id:number){
+    const url = 'http://localhost:3000/api/productos/eliminarProducto/';
+    const body = {id};
+    return this.http.delete<any>(url, {body});
   }
 }
