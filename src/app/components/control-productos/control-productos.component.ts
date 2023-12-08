dde89b1081d86548eaac3ed351306871ad9f1421
Cristian Lopez <analistadesarrollosapfront2_lem@castores.com.mx>
2023-12-08 09:09:07 -0600

    productos

--- src/app/components/control-productos/control-productos.component.ts
+++ src/app/components/control-productos/control-productos.component.ts
@@ -15,6 +15,10 @@ export class ControlProductosComponent {
 value2: any | undefined;
 items2: any[] = [];
 materias: any[] = [];
+// Mermas
+value3: any | undefined;
+items3: any[] = [];
+mermas: any[] = [];
 
   constructor(
     private productoService: ControlProductosService,
@@ -26,6 +30,9 @@ materias: any[] = [];
     this.configureItemsP();
     this.getMaterias();
     this.configureItemsMP();
+
+    this.getMermas();
+    this.configureItemsM();
   }
 
   configureItemsP() {
@@ -74,23 +81,15 @@ materias: any[] = [];
       this.productos = this.productos.filter((productos: any) =>
       productos.nombre_producto.toLowerCase().includes(this.value.toLowerCase())
     );
-    
+
     } else {
       this.getProductos();
     }
   }
-  
 
 
-
-
-
-
-
-
-  
   configureItemsMP() {
-    this.items = [
+    this.items2 = [
       {
         icon: 'pi pi-book',
         command: () => {
@@ -126,19 +125,73 @@ materias: any[] = [];
       },
       (error) => {
         console.error('Error al obtener las materias primas:', error);
-        this.productos = [];
+        this.materias = [];
       }
     );
   }
 
   applyFilter2() {
-    if (this.value) {
-      this.materias = this.materias.filter((venta: any) =>
-      venta.nombre.toLowerCase().includes(this.value.toLowerCase())
-    );
-    
+    if (this.value2) {
+      this.materias = this.materias.filter((materia: any) =>
+        materia.nombre.toLowerCase().includes(this.value2.toLowerCase())
+      );
     } else {
       this.getMaterias();
     }
   }
+
+
+
+  configureItemsM() {
+    this.items = [
+      {
+        icon: 'pi pi-book',
+        command: () => {
+          console.log('Habrir ');
+        },
+        tooltipOptions: {
+          tooltipLabel: 'Reporte',
+        },
+      },
+      {
+        icon: 'pi pi-chart-bar',
+        command: () => { },
+        tooltipOptions: {
+          tooltipLabel: 'Grafica de Barras',
+        },
+      },
+      {
+        icon: 'pi pi-chart-pie  custom-speed-dial-icon ',
+        command: () => { },
+        tooltipOptions: {
+          tooltipLabel: 'Grafica de Pastel',
+        },
+      },
+    ];
+  }
+
+  getMermas() {
+    this.productoService.getMermas().subscribe(
+      (response) => {
+        console.log(response)
+        this.mermas = response;
+      },
+      (error) => {
+        console.error('Error al obtener los productos:', error);
+        this.mermas = [];
+      }
+    );
+  }
+
+  applyFilter3() {
+    if (this.value) {
+      this.mermas = this.mermas.filter((mermas: any) =>
+      mermas.nombre.toLowerCase().includes(this.value.toLowerCase())
+    );
+
+    } else {
+      this.getMermas();
+    }
+  }
+
 }
